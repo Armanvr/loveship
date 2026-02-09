9df4780031e9314756ed87d1ac94e2f3e4ae7895
Arman OZTURK <arman090293@gmail.com>
2026-02-10 00:16:28 +0100

    Update .gitignore to exclude node_modules, public directory, and env files. Modify vite.config.ts to set output directory to 'public' and include Tailwind CSS plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import tailwindcss from '@tailwindcss/vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [preact(), tailwindcss()],
+	build: {
+		outDir: 'public',
+	},
 })
-
